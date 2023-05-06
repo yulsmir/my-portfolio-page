d91b7c0b1cc4113900834170f1649b24b5fd2500
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-06 07:42:46 +0200

    Fix it

--- app.js
+++ app.js
@@ -1,57 +1,45 @@
-// Sticky navbar on scroll script
-$(window).scroll(function () {
-  const scrollY = this.scrollY;
-  const navbar = $('.navbar');
-  const scrollUpBtn = $('.scroll-up-btn');
+$(document).ready(function () {
+  $(window).scroll(function (event) {
+    // sticky navbar on scroll script
+    if (event.scrollY > 20) {
+      $('.navbar').addClass('sticky');
+    } else {
+      $('.navbar').removeClass('sticky');
+    }
 
-  if (scrollY > 20) {
-    navbar.addClass('sticky');
-  } else {
-    navbar.removeClass('sticky');
-  }
+    // scroll-up button show/hide script
+    if (event.scrollY > 500) {
+      $('.scroll-up-btn').addClass('show');
+    } else {
+      $('.scroll-up-btn').removeClass('show');
+    }
+  });
 
-  if (scrollY > 500) {
-    scrollUpBtn.addClass('show');
-  } else {
-    scrollUpBtn.removeClass('show');
-  }
+  // slide-up script
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $('html').css('scrollBehavior', 'auto');
+  });
+
+  $('.navbar .menu li a').click(function () {
+    // applying again smooth scroll on menu items click
+    $('html').css('scrollBehavior', 'smooth');
+  });
+
+  // toggle menu/navbar script
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
+  });
+
+  // typing text animation script
+  const typed = new Typed('.typing', {
+    strings: ['Frontend', 'Backend', 'Developer'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 });
 
-// Scroll-up button script
-$('.scroll-up-btn').click(function () {
-  $('html').animate({ scrollTop: 0 }, 'slow');
-  $('html').css('scrollBehavior', 'auto');
-});
-
-// Smooth scroll on menu items click
-$('.navbar .menu li a').click(function () {
-  $('html').css('scrollBehavior', 'smooth');
-});
-
-// Toggle menu/navbar script
-$('.menu-btn').click(function () {
-  $('.navbar .menu').toggleClass('active');
-  $('.menu-btn i').toggleClass('active');
-});
-
-// Typing text animation script
-const typed = new Typed('.typing', {
-  strings: ['Frontend', 'Backend', 'Fullstack Developer'],
-  typeSpeed: 100,
-  backSpeed: 60,
-  loop: true,
-});
-
-// Project Tab scripts
-const openPage = (event, pageName) => {
-  const tabContent = $('.tab-content');
-  const tabLinks = $('.tab-link');
-
-  tabContent.hide();
-  tabLinks.removeClass('active');
-  $(`#${pageName}`).show();
-  $(event.currentTarget).addClass('active');
-};
-
-// Open default tab
-$('#default-open').click();
+document.getElementById('default-open').click();
